1728b893f96036a046de28ff66b25bb6a9e8149f
liuamin <liuamin@shengdaxd.com>
2018-08-02 17:42:23 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -9,26 +9,24 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // 设置统一插件
 const plugins = [
-	// new webpack.DefinePlugin({
-	// 	'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-	// }),
+	new webpack.DefinePlugin({
+		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+	}),
 	new MiniCssExtractPlugin({
 		filename: "static/css/[name].[hash].css",
 		disable: false,
 		allChunks: true
 	}),
-	// new HtmlWebpackPlugin({
-	// 	favicon: './src/favicon.ico',
-	// 	template: './src/index.html',
-	// 	inject: 'body',
-	// })
+	new HtmlWebpackPlugin({
+		favicon: './src/favicon.ico',
+		template: './src/index.html',
+		inject: 'body',
+	})
 ];
 // 非开发环境对js进行混淆，以及清除老版 dist 文件
-if (process.env.NODE_ENV !== 'DEV') {
-	// plugins.push(new UglifyJsPlugin(), new CleanWebpackPlugin(['dist']));
+if (process.env.NODE_ENV !== 'development') {
+	plugins.push(new UglifyJsPlugin(), new CleanWebpackPlugin(['dist']));
 }
-
-
 
 const config = {
 	mode: process.env.NODE_ENV,
@@ -37,10 +35,10 @@ const config = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		// publicPath: 'http://cdn.baidu.com/', // cdn 地址
+		publicPath: 'http://cdn.baidu.com/', // cdn 地址
 		publicPath: '/',
 		filename: 'static/js/[name].[hash].js', // 设置文件夹 和 文件的文字
-		// chunkFilename: 'static/js/[name][id].[chunkhash].js' // 针对多个 entry 点
+		chunkFilename: 'static/js/[name][id].[chunkhash].js' // 针对多个 entry 点
 	},
 	module: {
 		rules: [{
@@ -66,7 +64,7 @@ const config = {
 			use: {
 				loader: 'html-loader',
 				options: {
-					minimize: process.env.NODE_ENV !== 'DEV'
+					minimize: process.env.NODE_ENV !== 'development'
 				}
 			}
 		}],
