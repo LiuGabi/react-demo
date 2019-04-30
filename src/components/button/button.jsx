3fcffe290042c3ef096052a2b2500ceb4d74f704
liuamin <liuamin@shengdaxd.com>
2019-04-30 16:28:48 +0800

    update

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Button extends React.Component {
+	constructor(props) {
+		super(props);
+		console.log(super(props));
+	}
 	render() {
 		return <button>{this.props.text}</button>;
 	}
--- src/components/layout/content.jsx
+++ src/components/layout/content.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Button from '@/components/button/button.jsx';
+
 class Content extends React.Component {
 	render() {
-		return <div className='conent'>conent</div>;
+		return <div className='content'>
+				<Button text='我是按钮'></Button>
+		</div>;
 	}
 }
 
